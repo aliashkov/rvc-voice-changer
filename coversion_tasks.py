edd82c5cf262ea11694cb3136013e5619bb3acf7
aliashkov <art1699584@gmail.com>
2024-09-26 16:59:49 +0300

    #3 | feat: fix conversion

--- coversion_tasks.py
+++ coversion_tasks.py
@@ -3,19 +3,61 @@ import soundfile as sf
 import traceback
 import numpy as np
 from datetime import datetime
-from config import Config
+import traceback
+import numpy as np
+from flask import Flask, request, jsonify, send_file, abort
+import librosa
+import asyncio
+import edge_tts
+import soundfile as sf
+# import spaces
+from datetime import datetime
+from fairseq import checkpoint_utils
+from lib.infer_pack.models import (
+    SynthesizerTrnMs256NSFsid,
+    SynthesizerTrnMs256NSFsid_nono,
+    SynthesizerTrnMs768NSFsid,
+    SynthesizerTrnMs768NSFsid_nono,
+)
+from vc_infer_pipeline import VC
+from rq import Queue, Worker, Connection
+from rq.exceptions import NoSuchJobError
+from rq.job import Job
 from rq import get_current_job
 
-from converter import load_model  # Import from the new module
-
-config = Config()
+#config = Config()
 
 def perform_conversion(model_name, vc_audio_mode, vc_input, vc_upload, tts_text, tts_voice, f0_up_key, f0_method, index_rate, filter_radius, resample_sr, rms_mix_rate, protect):
-    job = get_current_job()  # Get the current job
-    job.meta['progress'] = 0  # Initialize progress
-    job.save_meta()  # Save initial job state
+        job = get_current_job()  # Get the current job
+        job.meta['progress'] = 0  # Initialize progress
+        job.save_meta()  # Save initial job state  
+        try:
+            print("Model name: ", model_name)
+            print("VC audio mode: ", vc_audio_mode)
+            print("VC Input: ", vc_input)
+            print("VC Upload: ", vc_upload)
+            print("TTS text: ", tts_text)
+            print("TTS voice: ", tts_voice)
+            print("F0 UP KEY: ", f0_up_key)
+            print("FO METHOD: ", f0_method)
+            print("Index rate: ", index_rate)
+            print("Filter rate: ", filter_radius)
+            print("Resample SR: ", resample_sr)
+            print("RMS MIX RATE: ", rms_mix_rate)
+            print("Protect: ", protect)
+            logs = []
+            print(f"Converting using {model_name}...")
+            logs.append(f"Converting using {model_name}...")
+        except:
+            info = traceback.format_exc()
+            print(info)
+            yield info, None
+""" return vc_fn """
 
-    """     print("Starting conversion...")
+    #categories = load_model(config)
+    #print(categories)
+
+"""     print("Starting conversion...")
 
     # Load categories and models
     categories = load_model(config)
@@ -49,11 +91,13 @@ def perform_conversion(model_name, vc_audio_mode, vc_input, vc_upload, tts_text,
     output_path = f"converted_{model_name}_{datetime.now().strftime('%Y%m%d%H%M%S')}.wav"
     sf.write(output_path, audio_opt, tgt_sr) """
 
-    job.meta['progress'] = 100  # Mark as complete
+"""     job.meta['progress'] = 100  # Mark as complete
+    print("65465456456")
     job.save_meta()  # Final update
+    print("56564654465465465645546") """
 
-    return {
-        "message": f"Successfully converted using {model_name}",
+"""     return {
+        "message": f"Successfully converted using",
         "info": "777",
         "output_path": "777777"
-    }
+    } """
